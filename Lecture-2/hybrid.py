a610fc6f9cdf3dd3e84e42e8e6ac168a73ab3a28
rogelioandrade <rogelio.a.mancisidor@bi.no>
2022-08-29 09:33:29 +0200

    adding 1 asnyc exercise from lecture 1

--- Lecture-2/hybrid.py
+++ Lecture-2/hybrid.py
@@ -7,14 +7,14 @@ NUM_YEARS = 5
 
 # Read input from the user.
 car_cost = float(input("Enter the cost of the car: "))
-miles_per_year = float(input("How many km will you drive each year? "))
+km_per_year = float(input("How many km will you drive each year? "))
 gas_price = float(input("Enter the price of gas per liter: "))
 efficiency = float(input("Enter the fuel efficiency in km/L: "))
 resale = float(input("How much can you sell the car for after 5 years? "))
 
 # Compute the total cost of ownership.
 total_cost = car_cost - resale + \
-             NUM_YEARS * miles_per_year / efficiency * gas_price
+             NUM_YEARS * km_per_year / efficiency * gas_price
 
 # Display the result.
 print("The total cost of ownership is %.2f." % total_cost)
