cfb5d88ff481ebcadca9680807f4004b43f6be67
S1044610 <144098336+S1044610@users.noreply.github.com>
2023-09-28 14:47:06 +0200

    Update argparsing_logreg.py

--- argparsing_logreg.py
+++ argparsing_logreg.py
@@ -5,10 +5,10 @@ def my_logistic_regression():
     parser = argparse.ArgumentParser(description='My logistic regression',
                                      epilog='you can go to: https://scikit-learn.org/stable/modules/generated/sklearn.linear_model.LogisticRegression.html')
     
-    parser.add_argument('--penalty', choices=["option1", "option2", "option3", "option4"], help='the penalty argument')
+    parser.add_argument('--penalty', choices=["l1", "l2", "elasticnet", "None"], help='the penalty argument,specify the norm of the penalty')
     parser.add_argument('--fit_intercept', action="store_true", help='specify fit_intercept as True by default')
     parser.add_argument('--max_iter', type=int, default=10, help='max_iter argument, default: 10')
-    parser.add_argument('--tol', type=int, default=1, help='tol argument, default: 1')
+    parser.add_argument('--tol', type=float, default=1e-4, help='tol argument, Tolerance for stopping criteria, default:1e-4')
 
     args = parser.parse_args()
 
